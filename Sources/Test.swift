badea61e5c2f24dd29ba76730caa1ca623b4fcfb
Guy Daher <guy.daher@algolia.com>
2017-03-27 16:06:16 +0200

    Dummy test to cocoapod

---
+++ Sources/Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  instantsearch
+//
+//  Created by Guy Daher on 27/03/2017.
+//  Copyright © 2017 Algolia. All rights reserved.
+//
+
+import Foundation
+
+public class Bob {
+    
+    public static func meaningOfLife() -> Int {
+        return 42
+    }
+    
+}
