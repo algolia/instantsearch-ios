1f2b3c243093fc8d541e88b8bfa8ef8db602cecb
Guy Daher <guy.daher@algolia.com>
2017-04-07 18:37:26 +0200

    comment(notes): Write implementation note to explain architecture and what are widgets

--- Sources/InstantSearchBinder.swift
+++ Sources/InstantSearchBinder.swift
@@ -9,9 +9,75 @@
 import Foundation
 import InstantSearchCore
 
+// ------------------------------------------------------------------------------------------------------
+// IMPLEMENTATION NOTES
+// ------------------------------------------------------------------------------------------------------
+//
+// # Architecture of InstantSearch
+//
+// InstantSearch is inspired by both MVVM and VIPER architecture. 
+//
+// This is an overview of the architecture:
+//
+// ```
+// View <--> ViewModel <--> Binder <--> Interactor/Model
+//                            |
+//                            V
+//                          Builder
+// ```
+//
+// Widgets can mean two things, depending on how modular you want your components to be:
+//
+//
+// 1. It can be the View
+//
+// ```
+// Widget <--> WidgetVM <--> Binder <--> Searcher
+//                             |
+//                             V
+//                           Builder
+// ```
+//
+// In this first case, we offer a better modular architecture where a WidgetVM can be reused
+// for different kind of widgets, for example: a collectionView and tableView can share
+// the same VM since the business logic is exactly the same, only the layout changes.
+// In that case, the Widget is independent of InstantSearchCore and WidgetVM is independent of UIKit.
+//
+//
+// 2. It can be the View and the ViewModel
+//
+// ```
+// Widget <--> Binder <--> Searcher
+//               |
+//               V
+//             Builder
+// ```
+//
+// In this second case, we offer an easier way to create new widgets since the widget has access
+// to the searcher and all of its method. The downside here is that we can't reuse the business logic
+// through a VM. The upside is that it's easy for 3rd party devs to create their own widgets and plug into IS.
+// In that case, the Widget is dependent on both InstantSearchCore and UIKit.
+//
+// We note that the View and the ViewModel depend on abstract delegates, which makes them reusable and testable.
+//
+// To explain the remaining 2 components:
+//
+// The Binder plays a role of exposing all possible search events, whether from the Searcher or other widgets,
+// and making them available for ViewModels or Views so that they can tune in.
+// In a way, it is like an observable that knows about all search events, and it will send the search events to 
+// the observers that decided to tune in. We decided to go with delegation to offer a clean safe interface.
+// It also makes sure to call the Builder methods on the Widgets that fit case 1 mentioned above
+//
+// The Builder plays the role of spinning up the ViewModels-Widgets for the View-Widgets,
+// takes care of injecting the concrete implementations into the View-Widgets and the ViewModel-Widgets,
+// and finally links the delegates of the View-Widget, ViewModel-Widget and the Searcher
+//
+// ------------------------------------------------------------------------------------------------------
+
+
 let clearAllFiltersNotification = Notification.Name(rawValue: "clearAllFiltersNotification")
 
-/// The Presenter and Binder
+/// Binds the Searcher to the widgets through delegation.
 @objc public class InstantSearchBinder : NSObject, SearcherDelegate {
     
     // MARK: - Properties
@@ -74,6 +140,13 @@ let clearAllFiltersNotification = Notification.Name(rawValue: "clearAllFiltersNo
     
     @objc public func add(widget: AlgoliaView) {
         
+        // Widgets that act as ViewModel and Views
+        // In that case, the widget is the delegate to the events emitted by the Searcher
+        
+        if let searchableWidget = widget as? SearchableViewModel {
+            searchableWidget.searcher = searcher
+        }
+        
         if let resultingWidget = widget as? ResultingDelegate {
             resultingDelegates.add(resultingWidget)
         }
@@ -94,10 +167,10 @@ let clearAllFiltersNotification = Notification.Name(rawValue: "clearAllFiltersNo
             refinableDelegateMap[attributeName]!.add(refinableWidget)
         }
         
-        
-        if let searchableWidget = widget as? SearchableViewModel {
-            searchableWidget.searcher = searcher
-        }
+        // Widgets that act only as Views
+        // We spin off a ViewModel associated to the particular View
+        // (e.g: HitsTableWidget will lead to spin off HitsViewModel)
+        // In that case, the ViewModel is the delegate to the events emitted by the Searcher
         
         if let hitWidget = widget as? HitsViewDelegate {
             
