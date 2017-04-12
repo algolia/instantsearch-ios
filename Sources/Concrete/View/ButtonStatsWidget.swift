9261a1b68072472552407e354696026f2974373c
Guy Daher <guy.daher@algolia.com>
2017-04-12 11:12:02 +0200

    feat(buttonsStatWidget): add button stats widget that can set the titleLabel without animation

---
+++ Sources/Concrete/View/ButtonStatsWidget.swift
@@ -0,0 +1,26 @@
+//
+//  ButtonStatsWidget.swift
+//  InstantSearch
+//
+//  Created by Guy Daher on 12/04/2017.
+//
+//
+
+import UIKit
+
+@IBDesignable
+@objc public class ButtonStatsWidget: UIButton, StatsViewDelegate, AlgoliaView {
+    
+    public var viewModel: StatsViewModelDelegate!
+    
+    @IBInspectable public var resultTemplate: String = "{nbHits} results"
+    @IBInspectable public var clearText: String = ""
+    @IBInspectable public var errorText: String = "Error in fetching results"
+    
+    public func set(text: String) {
+        UIView.performWithoutAnimation {
+            setTitle(text, for: .normal)
+            layoutIfNeeded()
+        }
+    }
+}
