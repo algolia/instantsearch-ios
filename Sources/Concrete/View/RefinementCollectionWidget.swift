910b08c09ac4f8e9863ca086cd8d03294eac03f6
Guy Daher <guy.daher@algolia.com>
2017-04-25 17:14:45 +0200

    feat(widget): create RefinementCollectionWidget

---
+++ Sources/Concrete/View/RefinementCollectionWidget.swift
@@ -0,0 +1,30 @@
+//
+//  RefinementCollectionWidget.swift
+//  ecommerce
+//
+//  Created by Guy Daher on 08/03/2017.
+//  Copyright © 2017 Guy Daher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+@objc public class RefinementCollectionWidget: UICollectionView, RefinementMenuViewDelegate, AlgoliaView {
+    
+    var viewModel: RefinementMenuViewModelDelegate!
+    
+    @IBInspectable public var attribute: String = ""
+    @IBInspectable public var refinedFirst: Bool = true
+    @IBInspectable public var `operator`: String = "or"
+    @IBInspectable public var sortBy: String = "count:desc"
+    @IBInspectable public var limit: Int = 10
+    
+    public func reloadRefinements() {
+        reloadData()
+    }
+    
+    func deselectRow(at indexPath: IndexPath) {
+        deselectItem(at: indexPath, animated: true)
+    }
+}
