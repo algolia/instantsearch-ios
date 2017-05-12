6397fb614693362e67b3d682fb8f603cb7d86b61
Guy Daher <guy.daher@algolia.com>
2017-05-12 14:22:01 +0200

    test(objc): add objectiveCBridging test Objective C compatibility

---
+++ Tests/ObjectiveCBridging.m
@@ -0,0 +1,39 @@
+//
+//  ObjectiveCBridging.m
+//  InstantSearch
+//
+//  Created by Guy Daher on 12/05/2017.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@import InstantSearch;
+
+
+/// Verifies that all the features are accessible from Objective-C.
+///
+/// Warning: This tests mostly **compilation**! The behavior is already tested in Swift test cases.
+///
+/// Note: Only the public API is tested here.
+@interface ObjectiveCBridging : XCTestCase
+
+@end
+
+
+@implementation ObjectiveCBridging
+
+- (void)setUp {
+        [super setUp];
+    }
+
+- (void)tearDown {
+        [super tearDown];
+    }
+
+- (void)testClient {
+    XCTAssertTrue(1==1);
+    XCTAssertTrue(2==2);
+}
+
+@end
