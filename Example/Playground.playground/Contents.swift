8e86be0ce66edb54e2b32ca46246d605da15879c
Guy Daher <guy.daher@algolia.com>
2017-05-22 19:13:37 +0200

    chore(playground): cleanup example

--- Example/Playground.playground/Contents.swift
+++ Example/Playground.playground/Contents.swift
@@ -9,9 +9,6 @@
 
 import UIKit
 import InstantSearch
-import InstantSearchCore
-import AlgoliaSearch
-
 
 var str = "Hello, playground"
 
@@ -19,23 +16,3 @@ var sliderWidget = SliderWidget()
 sliderWidget.attribute = "salePrice"
 
 
-var searcher: Searcher!
-var searchBar: UISearchBar!
-
-func viewDidLoad() {
-    super.viewDidLoad()
-    
-    let client = Client(appID: "appID", apiKey: "apiKey")
-    let index = client.index(withName: "indexName")
-    searcher = Searcher(index: index, resultHandler: resultHandler)
-    searchBar.delegate = self
-}
-
-func resultHandler(results: SearchResults?, error: Error?, userInfo: [String: Any]) {
-    print(results?.hits)
-}
-
-func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-    searcher.params.query = searchText
-    searcher.search()
-}
