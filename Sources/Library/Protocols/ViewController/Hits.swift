17b54b64823973e809773200294509e0d80cdd4d
Guy Daher <guy.daher@algolia.com>
2017-05-22 19:35:13 +0200

    doc(protocols): documentation for ViewController protocols

--- Sources/Library/Protocols/ViewController/Hits.swift
+++ Sources/Library/Protocols/ViewController/Hits.swift
@@ -9,21 +9,32 @@
 import Foundation
 import UIKit
 
-
+/// DataSource for a Table Hits Widget.
 @objc public protocol HitsTableViewDataSource: class {
+    
+    /// DataSource method called to specify the layout of a hit cell.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, containing hit: [String: Any]) -> UITableViewCell
 }
 
+/// Delegate for a Table Hits Widget.
 @objc public protocol HitsTableViewDelegate: class {
+    
+    /// Delegate method called when a hit cell is selected.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, containing hit: [String: Any])
 }
 
+/// DataSource for a Collection Hits Widget.
 @objc public protocol HitsCollectionViewDataSource: class {
+    
+    /// DataSource method called to specify the layout of a hit cell.
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath,
                         containing hit: [String: Any]) -> UICollectionViewCell
 }
 
+/// Delegate for a Collection Hits Widget.
 @objc public protocol HitsCollectionViewDelegate: class {
+    
+    /// Delegate method called when a hit cell is selected.
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath, containing hit: [String: Any])
 }
--- Sources/Library/Protocols/ViewController/Refinement.swift
+++ Sources/Library/Protocols/ViewController/Refinement.swift
@@ -9,7 +9,10 @@
 import Foundation
 import UIKit
 
+/// DataSource for a Table Refinement Menu Widget.
 @objc public protocol RefinementTableViewDataSource: class {
+    
+    /// DataSource method called to specify the layout of a facet cell.
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath,
                    containing facet: String,
@@ -17,7 +20,10 @@ import UIKit
                    is refined: Bool) -> UITableViewCell
 }
 
+/// Delegate for a Table Refinement Menu Widget.
 @objc public protocol RefinementTableViewDelegate: class {
+    
+    /// Delegate method called when a facet cell is selected.
     func tableView(_ tableView: UITableView,
                    didSelectRowAt indexPath: IndexPath,
                    containing facet: String,
@@ -25,7 +31,10 @@ import UIKit
                    is refined: Bool)
 }
 
+/// DataSource for a Collection Refinement Menu Widget.
 @objc public protocol RefinementCollectionViewDataSource: class {
+    
+    /// DataSource method called to specify the layout of a facet cell.
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath,
                         containing facet: String,
@@ -33,7 +42,10 @@ import UIKit
                         is refined: Bool) -> UICollectionViewCell
 }
 
+/// Delegate for a Collection Refinement Menu Widget.
 @objc public protocol RefinementCollectionViewDelegate: class {
+    
+    /// Delegate method called when a facet cell is selected.
     func collectionView(_ collectionView: UICollectionView,
                         didSelectItemAt indexPath: IndexPath,
                         containing facet: String,
