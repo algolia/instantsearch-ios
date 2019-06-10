faa9c425dd1fa6b2ac4297275fc6a3f2857c738d
Vladislav Fitc <vladislav.fitc@algolia.com>
2019-06-10 17:57:46 +0200

    Updated hits controller

--- Sources/Controller/Hits/HitsController.swift
+++ Sources/Controller/Hits/HitsController.swift
@@ -17,11 +17,13 @@ public typealias HitsWidget = InstantSearchCore.HitsController
 public class HitsController<Record: Codable>: NSObject {
 
   public let searcher: SingleIndexSearcher
+  public let filterState: FilterState
   public let viewModel: HitsViewModel<Record>
   public let onError: Observer<Error>
 
   public init<Widget: HitsWidget>(searcher: SingleIndexSearcher, viewModel: HitsViewModel<Record>, widget: Widget) where Widget.DataSource == HitsViewModel<Record> {
     self.searcher = searcher
+    self.filterState  = .init()
     self.viewModel = viewModel
     self.onError = Observer<Error>()
     widget.hitsSource = viewModel
@@ -29,8 +31,7 @@ public class HitsController<Record: Codable>: NSObject {
   
   public convenience init<Widget: HitsWidget>(index: Index, widget: Widget) where Widget.DataSource == HitsViewModel<Record> {
     let query = Query()
-    let filterState = FilterState()
-    let searcher = SingleIndexSearcher(index: index, query: query, filterState: filterState)
+    let searcher = SingleIndexSearcher(index: index, query: query)
 
     let viewModel = HitsViewModel<Record>()
     self.init(searcher: searcher, viewModel: viewModel, widget: widget)
