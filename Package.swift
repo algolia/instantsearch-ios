92748c791c4ee677882e7e2bf2e665f8440d5a54
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-01-29 17:18:53 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      .package(url:"https://github.com/algolia/instantsearch-core-swift", .branch("feat/search-suggestions")),
+      .package(url:"https://github.com/algolia/instantsearch-core-swift", .branch("develop")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
