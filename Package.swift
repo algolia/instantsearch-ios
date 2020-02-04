7b7fff6789f2b1dbbe3c11fccaed0c9f17ff5a70
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-02-04 13:45:44 +0100

    Add logging to dependencies

--- Package.swift
+++ Package.swift
@@ -24,11 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "InstantSearch",
-            dependencies: ["InstantSearchCore"],
+            dependencies: ["InstantSearchCore", "Logging"],
             path: "./Sources"),
         .testTarget(
             name: "InstantSearchTests",
-            dependencies: ["InstantSearch", "InstantSearchCore"],
+            dependencies: ["InstantSearch", "InstantSearchCore", "Logging"],
             path: "./Tests")
     ]
 )
