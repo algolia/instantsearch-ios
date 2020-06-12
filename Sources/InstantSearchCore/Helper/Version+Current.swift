e26fda8e748412cadd51fa7fe29ef12ed1f4ea2e
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-06-12 21:09:56 +0200

    Prepare for beta deployment

--- Sources/InstantSearchCore/Helper/Version+Current.swift
+++ Sources/InstantSearchCore/Helper/Version+Current.swift
@@ -1,2 +1,2 @@
 // This is generated file. Don't modify it manually.
-public extension Version { static let current: Version = .init(major: 7, minor: 0, patch: 0, prereleaseIdentifier: "beta.1") }
+public extension Version { static let current: Version = .init(major: 7, minor: 0, patch: 0, prereleaseIdentifier: "beta.0") }
