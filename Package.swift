6c2d218b3fce37d18f18a30a28ff0874383ee95a
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-07-15 21:07:00 +0200

    Update client dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             targets: ["InstantSearchCore"])
     ],
     dependencies: [
-      .package(name: "AlgoliaSearchClient", url:"https://github.com/algolia/algoliasearch-client-swift", from: "8.0.0-beta.12"),
+      .package(name: "AlgoliaSearchClient", url:"https://github.com/algolia/algoliasearch-client-swift", .branch("develop")),
       .package(name: "InstantSearchInsights", url:"https://github.com/algolia/instantsearch-ios-insights", from: "2.3.2")
     ],
     targets: [
