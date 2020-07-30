c4d3b6dcd80979e3df76aba986fd21a79ee2171c
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-07-30 11:31:14 +0200

    Add advanced Single/MultiIndex search connectors
    (cherry picked from commit ef85158719c5d0b5f7a7dde4e0baff7b0a5a7b9c)

---
+++ Sources/InstantSearch/AdvancedConnectors/SingleIndexSearchConnector+UIKit.swift
@@ -0,0 +1,51 @@
+//
+//  SingleIndexSearchConnector+UIKit.swift
+//  
+//
+//  Created by Vladislav Fitc on 30/07/2020.
+//
+
+#if !InstantSearchCocoaPods
+import InstantSearchCore
+#endif
+#if canImport(UIKit) && (os(iOS) || os(tvOS) || os(macOS))
+import UIKit
+
+public extension SingleIndexSearchConnector {
+  
+  @available(iOS 13.0, *)
+  init<HC: HitsController>(searcher: SingleIndexSearcher,
+                           searchController: UISearchController,
+                           hitsInteractor: HitsInteractor = .init(),
+                           hitsController: HC,
+                           filterState: FilterState? = nil)  where HC.DataSource == HitsInteractor {
+    let queryInputInteractor = QueryInputInteractor()
+    let textFieldController = TextFieldController(searchBar: searchController.searchBar)
+    self.init(searcher: searcher,
+              queryInputInteractor: queryInputInteractor,
+              queryInputController: textFieldController,
+              hitsInteractor: hitsInteractor,
+              hitsController: hitsController,
+              filterState: filterState)
+  }
+  
+  @available(iOS 13.0, *)
+  init<HC: HitsController>(appID: ApplicationID,
+                           apiKey: APIKey,
+                           indexName: IndexName,
+                           searchController: UISearchController,
+                           hitsInteractor: HitsInteractor = .init(),
+                           hitsController: HC,
+                           filterState: FilterState? = nil) where HC.DataSource == HitsInteractor {
+    let searcher = SingleIndexSearcher(appID: appID,
+                                       apiKey: apiKey,
+                                       indexName: indexName)
+    self.init(searcher: searcher,
+              searchController: searchController,
+              hitsInteractor: hitsInteractor,
+              hitsController: hitsController,
+              filterState: filterState)
+  }
+  
+}
+#endif
---
+++ Sources/InstantSearchCore/AdvancedConnectors/SingleIndexSearchConnector.swift
@@ -0,0 +1,84 @@
+//
+//  SingleIndexSearchConnector.swift
+//  DemoDirectory
+//
+//  Created by Vladislav Fitc on 23/07/2020.
+//  Copyright © 2020 Algolia. All rights reserved.
+//
+
+import Foundation
+
+public struct SingleIndexSearchConnector<Record: Codable>: Connection {
+  
+  public typealias HitsInteractor = InstantSearchCore.HitsInteractor<Record>
+  
+  public let hitsConnector: HitsConnector<Record>
+  public let hitsControllerConnection: Connection
+  
+  public let queryInputConnector: QueryInputConnector<SingleIndexSearcher>
+  public let queryInputControllerConnection: Connection
+  
+  public let filterStateHitsInteractorConnection: Connection?
+  public let filterStateSearcherConnection: Connection?
+  
+  public init<HC: HitsController, QI: QueryInputController>(searcher: SingleIndexSearcher,
+                                                            queryInputInteractor: QueryInputInteractor = .init(),
+                                                            queryInputController: QI,
+                                                            hitsInteractor: HitsInteractor = .init(),
+                                                            hitsController: HC,
+                                                            filterState: FilterState? = nil) where HC.DataSource == HitsInteractor {
+    hitsConnector = .init(searcher: searcher, interactor: hitsInteractor, filterState: filterState)
+    queryInputConnector = .init(searcher: searcher, interactor: queryInputInteractor)
+    
+    queryInputControllerConnection = queryInputInteractor.connectController(queryInputController)
+    hitsControllerConnection = hitsInteractor.connectController(hitsController)
+    
+    if let filterState = filterState {
+      filterStateHitsInteractorConnection = hitsInteractor.connectFilterState(filterState)
+      filterStateSearcherConnection = searcher.connectFilterState(filterState)
+    } else {
+      filterStateHitsInteractorConnection = nil
+      filterStateSearcherConnection = nil
+    }
+    
+  }
+  
+  public init<HC: HitsController, QI: QueryInputController>(appID: ApplicationID,
+                                                            apiKey: APIKey,
+                                                            indexName: IndexName,
+                                                            queryInputInteractor: QueryInputInteractor = .init(),
+                                                            queryInputController: QI,
+                                                            hitsInteractor: HitsInteractor = .init(),
+                                                            hitsController: HC,
+                                                            filterState: FilterState? = nil)  where HC.DataSource == HitsInteractor {
+    let searcher = SingleIndexSearcher(appID: appID,
+                                       apiKey: apiKey,
+                                       indexName: indexName)
+    self.init(searcher: searcher,
+              queryInputInteractor: queryInputInteractor,
+              queryInputController: queryInputController,
+              hitsInteractor: hitsInteractor,
+              hitsController: hitsController,
+              filterState: filterState)
+  }
+  
+  public func connect() {
+    disconnect()
+    queryInputConnector.connect()
+    queryInputControllerConnection.connect()
+    hitsConnector.connect()
+    hitsControllerConnection.connect()
+    filterStateSearcherConnection?.connect()
+    filterStateHitsInteractorConnection?.connect()
+  }
+  
+  public func disconnect() {
+    queryInputConnector.disconnect()
+    queryInputControllerConnection.disconnect()
+    hitsConnector.disconnect()
+    hitsControllerConnection.disconnect()
+    filterStateSearcherConnection?.disconnect()
+    filterStateHitsInteractorConnection?.disconnect()
+  }
+  
+}
