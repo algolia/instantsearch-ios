1497c559767a83ec123d2b1623061f5df1efe1f1
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-07-30 12:06:20 +0200

    Add HitsTable/CollectionViewController implementations
    (cherry picked from commit f9dd03fca07a97d1a99c6810b4f12c0bdf736be2)

---
+++ Sources/InstantSearch/Hits/TableView/HitsTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HitsTableViewController.swift
+//  DemoDirectory
+//
+//  Created by Vladislav Fitc on 29/07/2020.
+//  Copyright © 2020 Algolia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import InstantSearch
+
+open class HitsTableViewController<CellConfigurator: TableViewCellConfigurator>: UITableViewController, HitsController {
+    
+  public var hitsSource: HitsInteractor<CellConfigurator.Model>?
+  
+  open override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.register(CellConfigurator.Cell.self, forCellReuseIdentifier: CellConfigurator.cellIdentifier)
+  }
+
+  open override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return hitsSource?.numberOfHits() ?? 0
+  }
+  
+  open override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: CellConfigurator.cellIdentifier, for: indexPath)
+  }
+  
+  open override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    guard let cell = cell as? CellConfigurator.Cell else { return }
+    guard let model = hitsSource?.hit(atIndex: indexPath.row) else { return }
+    CellConfigurator(model: model, indexPath: indexPath).configure(cell)
+  }
+  
+  open override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    guard let model = hitsSource?.hit(atIndex: indexPath.row) else { return 0 }
+    return CellConfigurator(model: model, indexPath: indexPath).cellHeight
+  }
+  
+}
