9c65c87414a7dda03b60e2342c4e7c994e0395b2
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-09-01 16:41:01 +0200

    Improve FilterListConnector API. Add documentation and snippets

--- Sources/InstantSearchCore/FilterList/FilterListConnector.swift
+++ Sources/InstantSearchCore/FilterList/FilterListConnector.swift
@@ -8,12 +8,27 @@
 
 import Foundation
 
+public typealias FacetFilter = Filter.Facet
+public typealias NumericFilter = Filter.Numeric
+public typealias TagFilter = Filter.Tag
+
+/// Components that display a list of filters.
 public class FilterListConnector<Filter: FilterType & Hashable>: Connection {
 
+  /// Filter state that will hold your filters.
   public let filterState: FilterState
+  
+  /// The logic applied to the filters
   public let interactor: FilterListInteractor<Filter>
+  
+  /// Connection between interactor and filter state
   public let connectionFilterState: Connection
 
+  /// Initializer with external interactor
+  /// - Parameter filterState: Filter state that will hold your filters
+  /// - Parameter interactor: External filter list interactor
+  /// - Parameter operator: Filter group operator
+  /// - Parameter groupName: Filter group name
   public init(filterState: FilterState,
               interactor: FilterListInteractor<Filter>,
               `operator`: RefinementOperator,
@@ -24,7 +39,7 @@ public class FilterListConnector<Filter: FilterType & Hashable>: Connection {
                                                                operator: `operator`,
                                                                groupName: groupName)
   }
-
+  
   public func connect() {
     connectionFilterState.connect()
   }
--- Sources/InstantSearchCore/FilterList/TagFilterListConnector.swift
+++ Sources/InstantSearchCore/FilterList/TagFilterListConnector.swift
@@ -8,10 +8,16 @@
 
 import Foundation
 
+/// Filtering component that displays any kind of tag filters and lets the user refine the search results by selecting them.
 public typealias TagFilterListConnector = FilterListConnector<Filter.Tag>
 
 public extension TagFilterListConnector {
 
+  /// - Parameter tagFilters: The tag filters to display.
+  /// - Parameter selectionMode: Whether the list can have `single` or `multiple` selections.
+  /// - Parameter filterState: The filter state  that will hold your filters.
+  /// - Parameter operator: Whether we apply an `and` or `or` behavior to the filters in the filter state.
+  /// - Parameter groupName: Filter group name
   convenience init(tagFilters: [TagFilter] = [],
                    selectionMode: SelectionMode = .multiple,
                    filterState: FilterState,
