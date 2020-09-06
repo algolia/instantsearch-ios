7b5e814cedb64d5a8cd64ee53e40cfaaadb5c292
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-09-06 23:06:07 +0200

    Add documentation for clear filter

--- Sources/InstantSearchCore/FilterClear/FilterClearConnector.swift
+++ Sources/InstantSearchCore/FilterClear/FilterClearConnector.swift
@@ -8,29 +8,46 @@
 
 import Foundation
 
-public class FilterClearConnector: Connection {
-
+/// Component that clears all refinements that are currently active within the given FilterState
+public class FilterClearConnector {
+  
+  /// Filter state that will hold your filters
   public let filterState: FilterState
+  
+  /// Logic applied to the clear control
   public let interactor: FilterClearInteractor
+  
+  /// Connection between interactor and filter state
   public let filterStateConnection: Connection
-
-  init(filterState: FilterState,
-       interactor: FilterClearInteractor = .init(),
-       clearMode: ClearMode = .specified,
-       filterGroupIDs: [FilterGroup.ID]? = nil) {
+  
+  /**
+   - Parameters:
+     - filterState: Filter state that will hold your filters
+     - interactor: Logic applied to the Clear Refinements
+     - clearMode: Whether we should clear the specified filters or all filters except them
+     - filterGroupIDs: The groupIDs of filters to clear. All filters will be cleared if unspecified.
+   */
+  public init(filterState: FilterState,
+              interactor: FilterClearInteractor = .init(),
+              clearMode: ClearMode = .specified,
+              filterGroupIDs: [FilterGroup.ID]? = nil) {
     self.filterState = filterState
     self.interactor = interactor
     self.filterStateConnection = interactor.connectFilterState(filterState,
                                                                filterGroupIDs: filterGroupIDs,
                                                                clearMode: clearMode)
   }
+  
+}
 
+extension FilterClearConnector: Connection {
+  
   public func connect() {
     filterStateConnection.connect()
   }
-
+  
   public func disconnect() {
     filterStateConnection.disconnect()
   }
-
+  
 }
