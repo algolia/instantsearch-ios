169a396676016a7561c0171c76974b80a86ed54d
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-09-07 01:47:10 +0200

    Add snippets for filter list of each kind

---
+++ Tests/InstantSearchTests/Snippets/FacetFilterListSnippets.swift
@@ -0,0 +1,74 @@
+//
+//  FacetFilterListSnippets.swift
+//  
+//
+//  Created by Vladislav Fitc on 07/09/2020.
+//
+
+import Foundation
+import InstantSearch
+import UIKit
+
+class FacetFilterListSnippets {
+  
+  let interactor: FacetFilterListInteractor = .init()
+  let connector: FacetFilterListConnector = .init(filterState: .init(), operator: .and, groupName: "")
+  
+  func widgetExample() {
+    let searcher: SingleIndexSearcher = SingleIndexSearcher(appID: "YourApplicationID",
+                                                            apiKey: "YourSearchOnlyAPIKey",
+                                                            indexName: "YourIndexName")
+    
+    let filterState = FilterState()
+    
+    let filters: [Filter.Facet] = ["red", "blue", "green", "yellow", "black"].map {
+      .init(attribute: "color", stringValue: $0)
+    }
+    
+    let filterListConnector = FacetFilterListConnector(facetFilters: filters,
+                                                       selectionMode: .multiple,
+                                                       filterState: filterState,
+                                                       operator: .and,
+                                                       groupName: "Facet Filters")
+    
+    searcher.connectFilterState(filterState)
+    searcher.search()
+    
+    _ = filterListConnector
+
+  }
+  
+  func advancedExample() {
+    let searcher: SingleIndexSearcher = SingleIndexSearcher(appID: "YourApplicationID",
+                                                            apiKey: "YourSearchOnlyAPIKey",
+                                                            indexName: "YourIndexName")
+    
+    let filterState = FilterState()
+    
+    let filters: [Filter.Facet] = ["red", "blue", "green", "yellow", "black"].map {
+      .init(attribute: "color", stringValue: $0)
+    }
+    
+    let filterListInteractor = FacetFilterListInteractor(items: filters, selectionMode: .multiple)
+    
+    filterListInteractor.connectFilterState(filterState, operator: .and, groupName: "Facet Filters")
+    
+    searcher.connectFilterState(filterState)
+    searcher.search()
+  }
+  
+  func connectViewConnector() {
+    let filterListConnector: FacetFilterListConnector = /*...*/ self.connector
+    let filterListTableView: UITableView = .init()
+    let filterListController: FilterListTableController<FacetFilter> = .init(tableView: filterListTableView)
+    filterListConnector.interactor.connectController(filterListController)
+  }
+  
+  func connectViewInteractor() {
+    let filterListInteractor: FacetFilterListInteractor = /*...*/ self.interactor
+    let filterListTableView: UITableView = .init()
+    let filterListController: FilterListTableController<FacetFilter> = .init(tableView: filterListTableView)
+    filterListInteractor.connectController(filterListController)
+  }
+
+}
---
+++ Tests/InstantSearchTests/Snippets/TagFilterListSnippets.swift
@@ -0,0 +1,83 @@
+//
+//  TagFilterListSnippets.swift
+//  
+//
+//  Created by Vladislav Fitc on 07/09/2020.
+//
+
+import Foundation
+import InstantSearch
+import UIKit
+
+class TagFilterListSnippets {
+  
+  let interactor: TagFilterListInteractor = .init()
+  let connector: TagFilterListConnector = .init(filterState: .init(), operator: .and, groupName: "")
+  
+  func widgetExample() {
+    let searcher: SingleIndexSearcher = SingleIndexSearcher(appID: "YourApplicationID",
+                                                            apiKey: "YourSearchOnlyAPIKey",
+                                                            indexName: "YourIndexName")
+
+    let filterState = FilterState()
+    
+    let filters: [TagFilter] = [
+      "coupon",
+      "free shipping",
+      "free return",
+      "on sale",
+      "no exchange"
+    ]
+        
+    let filterListConnector = TagFilterListConnector(tagFilters: filters,
+                                                     selectionMode: .multiple,
+                                                     filterState: filterState,
+                                                     operator: .and,
+                                                     groupName: "Tag Filters")
+    
+    searcher.connectFilterState(filterState)
+    searcher.search()
+
+    _ = filterListConnector
+  }
+  
+  func advancedExample() {
+    
+    let searcher: SingleIndexSearcher = SingleIndexSearcher(appID: "YourApplicationID",
+                                                            apiKey: "YourSearchOnlyAPIKey",
+                                                            indexName: "YourIndexName")
+
+    let filterState = FilterState()
+    
+    let filters: [TagFilter] = [
+      "coupon",
+      "free shipping",
+      "free return",
+      "on sale",
+      "no exchange"
+    ]
+        
+    let filterListInteractor = TagFilterListInteractor(items: filters,
+                                                      selectionMode: .multiple)
+    
+    filterListInteractor.connectFilterState(filterState, operator: .and, groupName: "Tag Filters")
+    
+    searcher.connectFilterState(filterState)
+    searcher.search()
+  }
+  
+  func connectViewConnector() {
+    let filterListConnector: TagFilterListConnector = /*...*/ self.connector
+    let filterListTableView: UITableView = .init()
+    let filterListController: FilterListTableController<TagFilter> = .init(tableView: filterListTableView)
+    filterListConnector.interactor.connectController(filterListController)
+  }
+  
+  func connectViewInteractor() {
+    let filterListInteractor: TagFilterListInteractor = /*...*/ self.interactor
+    let filterListTableView: UITableView = .init()
+    let filterListController: FilterListTableController<TagFilter> = .init(tableView: filterListTableView)
+    filterListInteractor.connectController(filterListController)
+  }
+
+}
