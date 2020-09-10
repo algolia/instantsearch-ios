56257886adb601c6a8a918fd61745240f4943fc3
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-09-10 23:15:57 +0200

    Add convenient controller connectivity to FacetListConnector. Add convenient initializers with FacetSearcher

--- Sources/InstantSearchCore/FacetList/FacetListConnector.swift
+++ Sources/InstantSearchCore/FacetList/FacetListConnector.swift
@@ -29,38 +29,24 @@ public class FacetListConnector {
   /// Connection between interactor and searcher
   public let searcherConnection: Connection
   
-  /// Connection between interactor and controller
-  public let controllerConnection: Connection?
+  /// Connections between interactor and controllers
+  public var controllerConnections: [Connection]
   
-  internal init<Controller: FacetListController>(searcher: Searcher,
-                                                 filterState: FilterState = .init(),
-                                                 interactor: FacetListInteractor = .init(),
-                                                 controller: Controller,
-                                                 presenter: SelectableListPresentable?,
-                                                 attribute: Attribute,
-                                                 operator: RefinementOperator,
-                                                 groupName: String?) {
-    
-    self.filterState = filterState
-    self.searcher = searcher
-    self.interactor = interactor
-    self.attribute = attribute
-    
-    self.filterStateConnection = interactor.connectFilterState(filterState,
-                                                               with: attribute,
-                                                               operator: `operator`,
-                                                               groupName: groupName)
-    
-    self.controllerConnection = interactor.connectController(controller, with: presenter)
-    
-    switch searcher {
-    case .facet(let facetSearcher):
-      searcherConnection = interactor.connectFacetSearcher(facetSearcher)
-      
-    case .singleIndex(let singleIndexSearcher):
-      searcherConnection = interactor.connectSearcher(singleIndexSearcher, with: attribute)
-    }
-    
+  internal convenience init<Controller: FacetListController>(searcher: Searcher,
+                                                             filterState: FilterState = .init(),
+                                                             interactor: FacetListInteractor = .init(),
+                                                             controller: Controller,
+                                                             presenter: SelectableListPresentable?,
+                                                             attribute: Attribute,
+                                                             operator: RefinementOperator,
+                                                             groupName: String?) {
+    self.init(searcher: searcher,
+              filterState: filterState,
+              interactor: interactor,
+              attribute: attribute,
+              operator: `operator`,
+              groupName: groupName)
+    connectController(controller, with: presenter)
   }
   
   internal init(searcher: Searcher,
@@ -79,7 +65,7 @@ public class FacetListConnector {
                                                                operator: `operator`,
                                                                groupName: groupName)
     
-    self.controllerConnection = nil
+    self.controllerConnections = []
     
     switch searcher {
     case .facet(let facetSearcher):
@@ -91,12 +77,46 @@ public class FacetListConnector {
     
   }
   
-  /// Initializer with external interactor
+  @discardableResult func connectController<Controller: FacetListController>(_ controller: Controller,
+                                                                             with presenter: SelectableListPresentable? = nil,
+                                                                             externalReload: Bool = false) -> FacetList.ControllerConnection<Controller> {
+    let connection = interactor.connectController(controller, with: presenter)
+    controllerConnections.append(connection)
+    return connection
+  }
+    
+}
+
+/// Init with SingleIndexSearcher
+
+extension FacetListConnector {
+  
+  /// Init with explicit interactor
   /// - Parameter searcher: Searcher that handles your searches
   /// - Parameter filterState: Filter state that will hold your filters
   /// - Parameter attribute: Attribute to filter
   /// - Parameter interactor: External facet list interactor
-  /// - Parameter persistentSelection: When true, the selection will be kept even if it does not match current results anymore.
+  /// - Parameter operator: Filter group operator
+  /// - Parameter groupName: Filter group name
+  public convenience init(searcher: SingleIndexSearcher,
+                          filterState: FilterState = .init(),
+                          attribute: Attribute,
+                          interactor: FacetListInteractor = .init(),
+                          operator: RefinementOperator,
+                          groupName: String? = nil) {
+    self.init(searcher: .singleIndex(searcher),
+              filterState: filterState,
+              interactor: interactor,
+              attribute: attribute,
+              operator: `operator`,
+              groupName: groupName)
+  }
+  
+  /// Init with explicit interactor & controller
+  /// - Parameter searcher: Searcher that handles your searches
+  /// - Parameter filterState: Filter state that will hold your filters
+  /// - Parameter attribute: Attribute to filter
+  /// - Parameter interactor: External facet list interactor
   /// - Parameter operator: Filter group operator
   /// - Parameter groupName: Filter group name
   /// - Parameter controller: Controller presenting the facets
@@ -105,12 +125,10 @@ public class FacetListConnector {
                                                            filterState: FilterState = .init(),
                                                            attribute: Attribute,
                                                            interactor: FacetListInteractor = .init(),
-                                                           persistentSelection: Bool = false,
                                                            operator: RefinementOperator,
                                                            groupName: String? = nil,
                                                            controller: Controller,
                                                            presenter: SelectableListPresentable? = nil) {
-    searcher.keepSelectedEmptyFacets = persistentSelection
     self.init(searcher: .singleIndex(searcher),
               filterState: filterState,
               interactor: interactor,
@@ -121,37 +139,36 @@ public class FacetListConnector {
               groupName: groupName)
   }
   
-  /// Initializer with external interactor without controller
-  /// - Parameter searcher: Searcher that handles your searches
-  /// - Parameter filterState: Filter state that will hold your filters
-  /// - Parameter attribute: Attribute to filter
-  /// - Parameter interactor: External facet list interactor
-  /// - Parameter persistentSelection: When true, the selection will be kept even if it does not match current results anymore.
-  /// - Parameter operator: Filter group operator
-  /// - Parameter groupName: Filter group name
-  public convenience init(searcher: SingleIndexSearcher,
-                          filterState: FilterState = .init(),
-                          attribute: Attribute,
-                          interactor: FacetListInteractor = .init(),
-                          persistentSelection: Bool = false,
-                          operator: RefinementOperator,
-                          groupName: String? = nil) {
-    searcher.keepSelectedEmptyFacets = persistentSelection
-    self.init(searcher: .singleIndex(searcher),
-              filterState: filterState,
-              interactor: interactor,
-              attribute: attribute,
-              operator: `operator`,
-              groupName: groupName)
-  }
-  
-  /// Default initializer
+  /// Init with implicit interactor
   /// - Parameter searcher: Searcher that handles your searches
   /// - Parameter filterState: Filter state that will hold your filters
   /// - Parameter attribute: Attribute to filter
   /// - Parameter selectionMode: Whether the list can have single or multiple selections
   /// - Parameter facets: If specified, the default facet values to display.
-  /// - Parameter persistentSelection: When true, the selection will be kept even if it does not match current results anymore.
+  /// - Parameter operator: Filter group operator
+  /// - Parameter groupName: Identifier of the group of filters
+  public convenience init(searcher: SingleIndexSearcher,
+                          filterState: FilterState = .init(),
+                          attribute: Attribute,
+                          selectionMode: SelectionMode,
+                          facets: [Facet] = [],
+                          persistentSelection: Bool = false,
+                          operator: RefinementOperator,
+                          groupName: String? = nil) {
+    self.init(searcher: .singleIndex(searcher),
+              filterState: filterState,
+              interactor: .init(facets: facets, selectionMode: selectionMode),
+              attribute: attribute,
+              operator: `operator`,
+              groupName: groupName)
+  }
+
+  /// Init with implicit interactor & controller
+  /// - Parameter searcher: Searcher that handles your searches
+  /// - Parameter filterState: Filter state that will hold your filters
+  /// - Parameter attribute: Attribute to filter
+  /// - Parameter selectionMode: Whether the list can have single or multiple selections
+  /// - Parameter facets: If specified, the default facet values to display.
   /// - Parameter operator: Filter group operator
   /// - Parameter groupName: Identifier of the group of filters
   /// - Parameter controller: Controller presenting the facet list
@@ -166,7 +183,6 @@ public class FacetListConnector {
                                                            groupName: String? = nil,
                                                            controller: Controller,
                                                            presenter: SelectableListPresentable? = nil) {
-    searcher.keepSelectedEmptyFacets = persistentSelection
     self.init(searcher: .singleIndex(searcher),
               filterState: filterState,
               interactor: .init(facets: facets, selectionMode: selectionMode),
@@ -177,16 +193,69 @@ public class FacetListConnector {
               groupName: groupName)
   }
   
-  /// Default initializer without controller
-  /// - Parameter searcher: Searcher that handles your searches
+}
+
+/// Initializers with FacetSearcher
+
+extension FacetListConnector {
+  
+  /// Init with explicit interactor
+  /// - Parameter searcher: Searcher that handles your searches for facet values
+  /// - Parameter filterState: Filter state that will hold your filters
+  /// - Parameter attribute: Attribute to filter
+  /// - Parameter interactor: External facet list interactor
+  /// - Parameter operator: Filter group operator
+  /// - Parameter groupName: Filter group name
+  public convenience init(searcher: FacetSearcher,
+                          filterState: FilterState = .init(),
+                          attribute: Attribute,
+                          interactor: FacetListInteractor = .init(),
+                          operator: RefinementOperator,
+                          groupName: String? = nil) {
+    self.init(searcher: .facet(searcher),
+              filterState: filterState,
+              interactor: interactor,
+              attribute: attribute,
+              operator: `operator`,
+              groupName: groupName)
+  }
+  
+  /// Init with explicit interactor & controller
+  /// - Parameter searcher: Searcher that handles your searches for facet values
+  /// - Parameter filterState: Filter state that will hold your filters
+  /// - Parameter attribute: Attribute to filter
+  /// - Parameter interactor: External facet list interactor
+  /// - Parameter operator: Filter group operator
+  /// - Parameter groupName: Filter group name
+  /// - Parameter controller: Controller presenting the facets
+  /// - Parameter presenter: Presenter defining the appearance of the facet list
+  public convenience init<Controller: FacetListController>(searcher: FacetSearcher,
+                                                           filterState: FilterState = .init(),
+                                                           attribute: Attribute,
+                                                           interactor: FacetListInteractor = .init(),
+                                                           operator: RefinementOperator,
+                                                           groupName: String? = nil,
+                                                           controller: Controller,
+                                                           presenter: SelectableListPresentable? = nil) {
+    self.init(searcher: .facet(searcher),
+              filterState: filterState,
+              interactor: interactor,
+              controller: controller,
+              presenter: presenter,
+              attribute: attribute,
+              operator: `operator`,
+              groupName: groupName)
+  }
+  
+  /// Init with implicit interactor
+  /// - Parameter searcher: Searcher that handles your searches for facet values
   /// - Parameter filterState: Filter state that will hold your filters
   /// - Parameter attribute: Attribute to filter
   /// - Parameter selectionMode: Whether the list can have single or multiple selections
   /// - Parameter facets: If specified, the default facet values to display.
-  /// - Parameter persistentSelection: When true, the selection will be kept even if it does not match current results anymore.
   /// - Parameter operator: Filter group operator
   /// - Parameter groupName: Identifier of the group of filters
-  public convenience init(searcher: SingleIndexSearcher,
+  public convenience init(searcher: FacetSearcher,
                           filterState: FilterState = .init(),
                           attribute: Attribute,
                           selectionMode: SelectionMode,
@@ -194,14 +263,44 @@ public class FacetListConnector {
                           persistentSelection: Bool = false,
                           operator: RefinementOperator,
                           groupName: String? = nil) {
-    searcher.keepSelectedEmptyFacets = persistentSelection
-    self.init(searcher: .singleIndex(searcher),
+    self.init(searcher: .facet(searcher),
               filterState: filterState,
               interactor: .init(facets: facets, selectionMode: selectionMode),
               attribute: attribute,
               operator: `operator`,
               groupName: groupName)
   }
+
+  /// Init with implicit interactor & controller
+  /// - Parameter searcher: Searcher that handles your searches for facet values
+  /// - Parameter filterState: Filter state that will hold your filters
+  /// - Parameter attribute: Attribute to filter
+  /// - Parameter selectionMode: Whether the list can have single or multiple selections
+  /// - Parameter facets: If specified, the default facet values to display.
+  /// - Parameter operator: Filter group operator
+  /// - Parameter groupName: Identifier of the group of filters
+  /// - Parameter controller: Controller presenting the facet list
+  /// - Parameter presenter: Presenter defining the appearance of the facet list
+  public convenience init<Controller: FacetListController>(searcher: FacetSearcher,
+                                                           filterState: FilterState = .init(),
+                                                           attribute: Attribute,
+                                                           selectionMode: SelectionMode,
+                                                           facets: [Facet] = [],
+                                                           persistentSelection: Bool = false,
+                                                           operator: RefinementOperator,
+                                                           groupName: String? = nil,
+                                                           controller: Controller,
+                                                           presenter: SelectableListPresentable? = nil) {
+    self.init(searcher: .facet(searcher),
+              filterState: filterState,
+              interactor: .init(facets: facets, selectionMode: selectionMode),
+              controller: controller,
+              presenter: presenter,
+              attribute: attribute,
+              operator: `operator`,
+              groupName: groupName)
+  }
+
   
 }
 
@@ -210,13 +309,13 @@ extension FacetListConnector: Connection {
   public func connect() {
     filterStateConnection.connect()
     searcherConnection.connect()
-    controllerConnection?.connect()
+    controllerConnections.forEach { $0.connect() }
   }
   
   public func disconnect() {
     filterStateConnection.disconnect()
     searcherConnection.disconnect()
-    controllerConnection?.disconnect()
+    controllerConnections.forEach { $0.disconnect() }
   }
 
 }
