1837d3e8a6ff7ffa9d7309f5f3c374831bc6a803
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-09-14 22:24:48 +0200

    Add convenient controller connectivity to query input interactor

--- Sources/InstantSearchCore/QueryInput/QueryInputConnector.swift
+++ Sources/InstantSearchCore/QueryInput/QueryInputConnector.swift
@@ -19,6 +19,9 @@ public class QueryInputConnector<S: Searcher> {
 
   /// Connection between query input interactor and searcher
   public let searcherConnection: Connection
+  
+  /// Connections between interactor and controllers
+  public var controllerConnections: [Connection]
 
   /**
    - Parameters:
@@ -32,8 +35,42 @@ public class QueryInputConnector<S: Searcher> {
     self.searcher = searcher
     self.interactor = interactor
     self.searcherConnection = interactor.connectSearcher(searcher, searchTriggeringMode: searchTriggeringMode)
+    self.controllerConnections = []
   }
 
+}
+
+extension QueryInputConnector {
+  
+  /**
+   - Parameters:
+     - searcher: Searcher that handles your searches
+     - interactor: Business logic that handles new search inputs
+     - searchTriggeringMode: Defines the event triggering a new search
+     - controller: Controller that interfaces with a concrete query input view
+   */
+  public convenience init<Controller: QueryInputController>(searcher: S,
+              interactor: QueryInputInteractor = .init(),
+              searchTriggeringMode: SearchTriggeringMode = .searchAsYouType,
+              controller: Controller) {
+    self.init(searcher: searcher,
+              interactor: interactor,
+              searchTriggeringMode: searchTriggeringMode)
+    connectController(controller)
+  }
+  
+  /**
+   Establishes a connection with the controller
+   - Parameters:
+     - controller: Controller that interfaces with a concrete query input view
+   - Returns: Established connection
+  */
+  @discardableResult func connectController<Controller: QueryInputController>(_ controller: Controller) -> some Connection {
+    let connection = interactor.connectController(controller)
+    controllerConnections.append(connection)
+    return connection
+  }
+  
 }
 
 extension QueryInputConnector: Connection {
