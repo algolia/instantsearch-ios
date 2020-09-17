d78da5a1e28e0bfc3269647ab7d70a2b3990c057
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-09-17 14:40:58 +0200

    Improve structure and documentation of FacetList connector

---
+++ Sources/InstantSearchCore/FacetList/Connector/FacetListConnector+Controller.swift
@@ -0,0 +1,47 @@
+//
+//  FacetListConnector+Controller.swift
+//  
+//
+//  Created by Vladislav Fitc on 17/09/2020.
+//
+
+import Foundation
+
+//MARK: - Convenient controller connectivity
+
+public extension FacetListConnector {
+  
+  internal convenience init<Controller: FacetListController>(searcher: Searcher,
+                                                             filterState: FilterState = .init(),
+                                                             interactor: FacetListInteractor = .init(),
+                                                             controller: Controller,
+                                                             presenter: SelectableListPresentable?,
+                                                             attribute: Attribute,
+                                                             operator: RefinementOperator,
+                                                             groupName: String?) {
+    self.init(searcher: searcher,
+              filterState: filterState,
+              interactor: interactor,
+              attribute: attribute,
+              operator: `operator`,
+              groupName: groupName)
+    connectController(controller, with: presenter)
+  }
+  
+  /**
+   Establishes a connection with the controller using the provided presentation logic
+   - Parameters:
+     - controller: Controller interfacing with a concrete facet list view
+     - presenter: Presenter defining how a facet appears in the controller
+     - externalReload: Defines if controller will be updated automatically by the events or manually
+   - Returns: Established connection
+  */
+  @discardableResult func connectController<Controller: FacetListController>(_ controller: Controller,
+                                                                             with presenter: SelectableListPresentable? = nil,
+                                                                             externalReload: Bool = false) -> some Connection {
+    let connection = interactor.connectController(controller, with: presenter)
+    controllerConnections.append(connection)
+    return connection
+  }
+  
+}
---
+++ Sources/InstantSearchCore/FacetList/Connector/FacetListConnector.swift
@@ -0,0 +1,88 @@
+//
+//  FacetListConnector.swift
+//  InstantSearchCore
+//
+//  Created by Vladislav Fitc on 26/11/2019.
+//  Copyright © 2019 Algolia. All rights reserved.
+//
+
+import Foundation
+
+/// Component that perform refinements on a facet.
+public class FacetListConnector {
+  
+  /// Searcher that handles your searches.
+  public let searcher: Searcher
+  
+  /// Filter state holding your filters
+  public let filterState: FilterState
+  
+  /// Logic applied to the facets
+  public let interactor: FacetListInteractor
+  
+  /// Attribute to filter
+  public let attribute: Attribute
+  
+  /// Connection between interactor and filter state
+  public let filterStateConnection: Connection
+  
+  /// Connection between interactor and searcher
+  public let searcherConnection: Connection
+  
+  /// Connections between interactor and controllers
+  public var controllerConnections: [Connection]
+  
+  internal init(searcher: Searcher,
+                filterState: FilterState = .init(),
+                interactor: FacetListInteractor = .init(),
+                attribute: Attribute,
+                operator: RefinementOperator,
+                groupName: String?) {
+    self.filterState = filterState
+    self.searcher = searcher
+    self.interactor = interactor
+    self.attribute = attribute
+    
+    self.filterStateConnection = interactor.connectFilterState(filterState,
+                                                               with: attribute,
+                                                               operator: `operator`,
+                                                               groupName: groupName)
+    
+    self.controllerConnections = []
+    
+    switch searcher {
+    case .facet(let facetSearcher):
+      searcherConnection = interactor.connectFacetSearcher(facetSearcher)
+      
+    case .singleIndex(let singleIndexSearcher):
+      searcherConnection = interactor.connectSearcher(singleIndexSearcher, with: attribute)
+    }
+    
+  }
+    
+}
+
+extension FacetListConnector: Connection {
+  
+  public func connect() {
+    filterStateConnection.connect()
+    searcherConnection.connect()
+    controllerConnections.forEach { $0.connect() }
+  }
+  
+  public func disconnect() {
+    filterStateConnection.disconnect()
+    searcherConnection.disconnect()
+    controllerConnections.forEach { $0.disconnect() }
+  }
+
+}
+
+extension FacetListConnector {
+  
+  public enum Searcher {
+    case singleIndex(SingleIndexSearcher)
+    case facet(FacetSearcher)
+  }
+  
+}
