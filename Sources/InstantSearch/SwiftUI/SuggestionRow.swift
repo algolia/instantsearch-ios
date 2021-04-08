9f2a0b188e120f261632053941860c7d709bfee6
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-04-08 16:00:23 +0200

    Add SuggestionRow

---
+++ Sources/InstantSearch/SwiftUI/SuggestionRow.swift
@@ -0,0 +1,42 @@
+//
+//  SuggestionRow.swift
+//  
+//
+//  Created by Vladislav Fitc on 01/04/2021.
+//
+
+import Foundation
+import SwiftUI
+
+public struct SuggestionRow: View {
+
+  public let text: String
+  public var onTypeAhead: (String) -> Void
+  public var onSelection: (String) -> Void
+  
+  public init(text: String,
+              onSelection: @escaping (String) -> Void,
+              onTypeAhead: @escaping (String) -> Void) {
+    self.text = text
+    self.onSelection = onSelection
+    self.onTypeAhead = onTypeAhead
+  }
+  
+  public var body: some View {
+    HStack {
+      Text(text)
+        .padding(.vertical, 3)
+      Spacer()
+      Button(action: { onTypeAhead(text) }) {
+        Image(systemName: "arrow.up.backward").foregroundColor(.gray)
+      }
+    }
+    .padding(.vertical, 4)
+    .padding(.horizontal, 20)
+    .contentShape(Rectangle())
+    .onTapGesture {
+      onSelection(text)
+    }
+  }
+  
+}
