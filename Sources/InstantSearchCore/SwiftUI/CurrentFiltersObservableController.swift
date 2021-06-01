9867035607cc763873a3eb4c26aa55d449653114
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-06-01 13:31:41 +0200

    implement current filters observable controller

--- Sources/InstantSearchCore/SwiftUI/CurrentFiltersObservableController.swift
+++ Sources/InstantSearchCore/SwiftUI/CurrentFiltersObservableController.swift
@@ -6,14 +6,17 @@
 //
 
 import Foundation
+#if canImport(Combine) && canImport(SwiftUI)
+import Combine
+import SwiftUI
 
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public class CurrentFiltersObservableController: ObservableObject, CurrentFiltersController {
 
-  @Published public var isEmpty: Bool
+  @Published public var filters: [FilterAndID]
 
-  public func setItems(_ items: [FilterAndID]) {
-    isEmpty = items.isEmpty
+  public func setItems(_ filters: [FilterAndID]) {
+    self.filters = filters
   }
 
   public var onRemoveItem: ((FilterAndID) -> Void)?
@@ -22,8 +25,9 @@ public class CurrentFiltersObservableController: ObservableObject, CurrentFilter
     objectWillChange.send()
   }
 
-  public init(isEmpty: Bool = true) {
-    self.isEmpty = isEmpty
+  public init(filters: [FilterAndID] = []) {
+    self.filters = filters
   }
 
 }
+#endif
