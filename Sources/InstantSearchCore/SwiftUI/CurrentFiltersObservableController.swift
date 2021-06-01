3ce60493d15affa042bf90b1b43d027783e654d6
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-06-01 19:16:57 +0200

    add doc for current filters observable controller

--- Sources/InstantSearchCore/SwiftUI/CurrentFiltersObservableController.swift
+++ Sources/InstantSearchCore/SwiftUI/CurrentFiltersObservableController.swift
@@ -10,9 +10,11 @@ import Foundation
 import Combine
 import SwiftUI
 
+/// CurrentFiltersController implementation adapted for usage with SwiftUI views
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public class CurrentFiltersObservableController: ObservableObject, CurrentFiltersController {
 
+  /// Currently applied filters with an associated filter group ID
   @Published public var filters: [FilterAndID]
 
   public func setItems(_ filters: [FilterAndID]) {
