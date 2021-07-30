f53353b720f52580723473d5b4171826c7baaa57
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-07-30 19:18:08 +0200

    restore MultiIndexSearchConnector & MultiIndexHitsConnector

--- Sources/InstantSearchCore/AdvancedConnectors/MultiIndexSearchConnector.swift
+++ Sources/InstantSearchCore/AdvancedConnectors/MultiIndexSearchConnector.swift
@@ -20,80 +20,80 @@ Connector encapsulating basic search experience within multiple indices
  - searcher <-> filter states (if provided)
 Most of the components associated by this connector are created and connected automatically, it's only required to provide a proper `Controller` implementations.
 */
-//public struct MultiIndexSearchConnector: Connection {
-//
-//  /// Connector establishing the linkage between searcher, hits interactor and optionally filter state
-//  public let hitsConnector: MultiIndexHitsConnector
-//
-//  /// Connection between hits interactor of hits connector and provided hits controller
-//  public let hitsControllerConnection: Connection
-//
-//  /// Connector establishing the linkage between searcher and query input interactor
-//  public let queryInputConnector: QueryInputConnector
-//
-//  /// Connection between query input interactor of query input connector and provided query input controller
-//  public let queryInputControllerConnection: Connection
-//
-//  /**
-//   - Parameters:
-//     - searcher: External multi index sercher
-//     - indexModules: List of index modules associating index name, hits interactor and optional filter state
-//     - hitsController: Hits controller
-//     - queryInputInteractor: External query input interactor
-//     - queryInputController: Query input controller
-//   */
-//  public init<QI: QueryInputController, HC: MultiIndexHitsController>(searcher: MultiIndexSearcher,
-//                                                                      indexModules: [MultiIndexHitsConnector.IndexModule],
-//                                                                      hitsController: HC,
-//                                                                      queryInputInteractor: QueryInputInteractor = .init(),
-//                                                                      queryInputController: QI) {
-//    let hitsInteractor = MultiIndexHitsInteractor(hitsInteractors: indexModules.map(\.hitsInteractor))
-//    let filterStates = indexModules.map(\.filterState)
-//    self.hitsConnector = .init(searcher: searcher, interactor: hitsInteractor, filterStates: filterStates)
-//    hitsControllerConnection = self.hitsConnector.interactor.connectController(hitsController)
-//    self.queryInputConnector = .init(searcher: searcher, interactor: queryInputInteractor)
-//    queryInputControllerConnection = queryInputInteractor.connectController(queryInputController)
-//    searcher.search()
-//  }
-//
-//  /**
-//   - Parameters:
-//     - appID: Application ID
-//     - apiKey: API Key
-//     - indexModules: List of index modules associating index name, hits interactor and optional filter state
-//     - hitsController: Hits controller
-//     - queryInputInteractor: External query input interactor
-//     - queryInputController: Query input controller
-//   */
-//  public init<QI: QueryInputController, HC: MultiIndexHitsController>(appID: ApplicationID,
-//                                                                      apiKey: APIKey,
-//                                                                      indexModules: [MultiIndexHitsConnector.IndexModule],
-//                                                                      hitsController: HC,
-//                                                                      queryInputInteractor: QueryInputInteractor = .init(),
-//                                                                      queryInputController: QI) {
-//    let searcher = MultiIndexSearcher(appID: appID,
-//                                      apiKey: apiKey,
-//                                      indexNames: indexModules.map(\.indexName))
-//    self.init(searcher: searcher,
-//              indexModules: indexModules,
-//              hitsController: hitsController,
-//              queryInputInteractor: queryInputInteractor,
-//              queryInputController: queryInputController)
-//  }
-//
-//  public func connect() {
-//    disconnect()
-//    hitsConnector.connect()
-//    hitsControllerConnection.connect()
-//    queryInputConnector.connect()
-//    queryInputControllerConnection.connect()
-//  }
-//
-//  public func disconnect() {
-//    hitsConnector.disconnect()
-//    hitsControllerConnection.disconnect()
-//    queryInputConnector.disconnect()
-//    queryInputControllerConnection.disconnect()
-//  }
-//
-//}
+public struct MultiIndexSearchConnector: Connection {
+
+  /// Connector establishing the linkage between searcher, hits interactor and optionally filter state
+  public let hitsConnector: MultiIndexHitsConnector
+
+  /// Connection between hits interactor of hits connector and provided hits controller
+  public let hitsControllerConnection: Connection
+
+  /// Connector establishing the linkage between searcher and query input interactor
+  public let queryInputConnector: QueryInputConnector
+
+  /// Connection between query input interactor of query input connector and provided query input controller
+  public let queryInputControllerConnection: Connection
+
+  /**
+   - Parameters:
+     - searcher: External multi index sercher
+     - indexModules: List of index modules associating index name, hits interactor and optional filter state
+     - hitsController: Hits controller
+     - queryInputInteractor: External query input interactor
+     - queryInputController: Query input controller
+   */
+  public init<QI: QueryInputController, HC: MultiIndexHitsController>(searcher: MultiIndexSearcher,
+                                                                      indexModules: [MultiIndexHitsConnector.IndexModule],
+                                                                      hitsController: HC,
+                                                                      queryInputInteractor: QueryInputInteractor = .init(),
+                                                                      queryInputController: QI) {
+    let hitsInteractor = MultiIndexHitsInteractor(hitsInteractors: indexModules.map(\.hitsInteractor))
+    let filterStates = indexModules.map(\.filterState)
+    self.hitsConnector = .init(searcher: searcher, interactor: hitsInteractor, filterStates: filterStates)
+    hitsControllerConnection = self.hitsConnector.interactor.connectController(hitsController)
+    self.queryInputConnector = .init(searcher: searcher, interactor: queryInputInteractor)
+    queryInputControllerConnection = queryInputInteractor.connectController(queryInputController)
+    searcher.search()
+  }
+
+  /**
+   - Parameters:
+     - appID: Application ID
+     - apiKey: API Key
+     - indexModules: List of index modules associating index name, hits interactor and optional filter state
+     - hitsController: Hits controller
+     - queryInputInteractor: External query input interactor
+     - queryInputController: Query input controller
+   */
+  public init<QI: QueryInputController, HC: MultiIndexHitsController>(appID: ApplicationID,
+                                                                      apiKey: APIKey,
+                                                                      indexModules: [MultiIndexHitsConnector.IndexModule],
+                                                                      hitsController: HC,
+                                                                      queryInputInteractor: QueryInputInteractor = .init(),
+                                                                      queryInputController: QI) {
+    let searcher = MultiIndexSearcher(appID: appID,
+                                      apiKey: apiKey,
+                                      indexNames: indexModules.map(\.indexName))
+    self.init(searcher: searcher,
+              indexModules: indexModules,
+              hitsController: hitsController,
+              queryInputInteractor: queryInputInteractor,
+              queryInputController: queryInputController)
+  }
+
+  public func connect() {
+    disconnect()
+    hitsConnector.connect()
+    hitsControllerConnection.connect()
+    queryInputConnector.connect()
+    queryInputControllerConnection.connect()
+  }
+
+  public func disconnect() {
+    hitsConnector.disconnect()
+    hitsControllerConnection.disconnect()
+    queryInputConnector.disconnect()
+    queryInputControllerConnection.disconnect()
+  }
+
+}
--- Sources/InstantSearchCore/MultiIndexHits/Connector/MultiIndexHitsConnector+Controller.swift
+++ Sources/InstantSearchCore/MultiIndexHits/Connector/MultiIndexHitsConnector+Controller.swift
@@ -7,39 +7,39 @@
 
 import Foundation
 
-//public extension MultiIndexHitsConnector {
-//
-//  /**
-//   - Parameters:
-//     - appID: ID of your application
-//     - apiKey: Your application API Key
-//     - indexModules: List of components representing the single index, containing its name, hits interactor and an optional filter state
-//     - controller: Controller interfacing with a concrete multi-index hits view
-//  */
-//  convenience init<Controller: MultiIndexHitsController>(appID: ApplicationID,
-//                                                         apiKey: APIKey,
-//                                                         indexModules: [IndexModule],
-//                                                         controller: Controller) {
-//    let searcher = MultiIndexSearcher(appID: appID,
-//                                      apiKey: apiKey,
-//                                      indexNames: indexModules.map { $0.indexName })
-//    let interactor = MultiIndexHitsInteractor(hitsInteractors: indexModules.map { $0.hitsInteractor })
-//    self.init(searcher: searcher,
-//              interactor: interactor,
-//              filterStates: indexModules.map { $0.filterState })
-//    connectController(controller)
-//  }
-//
-//  /**
-//   Establishes a connection with the controller
-//   - Parameters:
-//     - controller: Controller interfacing with a concrete multi-index hits view
-//   - Returns: Established connection
-//  */
-//  @discardableResult func connectController<Controller: MultiIndexHitsController>(_ controller: Controller) -> MultiIndexHitsInteractor.ControllerConnection<Controller> {
-//    let connection = interactor.connectController(controller)
-//    connection.connect()
-//    return connection
-//  }
-//
-//}
+public extension MultiIndexHitsConnector {
+
+  /**
+   - Parameters:
+     - appID: ID of your application
+     - apiKey: Your application API Key
+     - indexModules: List of components representing the single index, containing its name, hits interactor and an optional filter state
+     - controller: Controller interfacing with a concrete multi-index hits view
+  */
+  convenience init<Controller: MultiIndexHitsController>(appID: ApplicationID,
+                                                         apiKey: APIKey,
+                                                         indexModules: [IndexModule],
+                                                         controller: Controller) {
+    let searcher = MultiIndexSearcher(appID: appID,
+                                      apiKey: apiKey,
+                                      indexNames: indexModules.map { $0.indexName })
+    let interactor = MultiIndexHitsInteractor(hitsInteractors: indexModules.map { $0.hitsInteractor })
+    self.init(searcher: searcher,
+              interactor: interactor,
+              filterStates: indexModules.map { $0.filterState })
+    connectController(controller)
+  }
+
+  /**
+   Establishes a connection with the controller
+   - Parameters:
+     - controller: Controller interfacing with a concrete multi-index hits view
+   - Returns: Established connection
+  */
+  @discardableResult func connectController<Controller: MultiIndexHitsController>(_ controller: Controller) -> MultiIndexHitsInteractor.ControllerConnection<Controller> {
+    let connection = interactor.connectController(controller)
+    connection.connect()
+    return connection
+  }
+
+}
