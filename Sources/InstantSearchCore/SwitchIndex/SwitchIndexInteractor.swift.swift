13865a3f93393c80292f512f62c32b882dcd9f19
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-11-05 16:50:58 +0100

    restore SwitchIndex component

---
+++ Sources/InstantSearchCore/SwitchIndex/SwitchIndexInteractor.swift.swift
@@ -0,0 +1,29 @@
+//
+//  SwitchIndexInteractor.swift
+//
+//
+//  Created by Vladislav Fitc on 08/04/2021.
+//
+import Foundation
+
+public class SwitchIndexInteractor {
+
+  public var indexNames: [IndexName]
+
+  public var selectedIndexName: IndexName {
+    didSet {
+      guard oldValue != selectedIndexName else { return }
+      onSelectionChange.fire(selectedIndexName)
+    }
+  }
+
+  public var onSelectionChange: Observer<IndexName>
+
+  public init(indexNames: [IndexName], selectedIndexName: IndexName) {
+    assert(indexNames.contains(selectedIndexName))
+    self.indexNames = indexNames
+    self.selectedIndexName = selectedIndexName
+    self.onSelectionChange = .init()
+  }
+
+}
---
+++ Sources/InstantSearchSwiftUI/DataModel/SwitchIndexObservableController.swift
@@ -0,0 +1,38 @@
+//
+//  SwitchIndexObservableController.swift
+//
+//
+//  Created by Vladislav Fitc on 01/04/2021.
+//
+#if !InstantSearchCocoaPods
+import InstantSearchCore
+#endif
+#if canImport(Combine) && canImport(SwiftUI) && (arch(arm64) || arch(x86_64))
+import Combine
+import SwiftUI
+
+/// SwitchIndexController implementation adapted for usage with SwiftUI views
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public class SwitchIndexObservableController: ObservableObject, SwitchIndexController {
+
+  /// List of indices names to switch between
+  @Published public var indexNames: [IndexName]
+
+  /// Name of currently selected index
+  @Published public var selected: IndexName
+
+  public var select: (IndexName) -> Void = { _ in }
+
+  public func set(indexNames: [IndexName], selected: IndexName) {
+    self.indexNames = indexNames
+    self.selected = selected
+  }
+
+  public init(indexNames: [IndexName] = [],
+              selected: IndexName = "") {
+    self.indexNames = indexNames
+    self.selected = selected
+  }
+
+}
+#endif
