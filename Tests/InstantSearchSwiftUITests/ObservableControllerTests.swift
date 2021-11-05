e78e712b8ad8a703631ae34afb817c85cfd17beb
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-11-05 20:32:49 +0100

    add SwiftUI test source to supress warning

---
+++ Tests/InstantSearchSwiftUITests/ObservableControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ObservableControllerTests.swift
+//  
+//
+//  Created by Vladislav Fitc on 05/11/2021.
+//
+
+import XCTest
+@testable import InstantSearchSwiftUI
+
+class ObservableControllerTests: XCTestCase {
+
+  
+      
+}
