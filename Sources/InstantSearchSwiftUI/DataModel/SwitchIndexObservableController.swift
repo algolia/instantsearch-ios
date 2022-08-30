87f2f24fbea928588e78b50755a7c283a43f3ef4
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-08-30 18:03:52 +0200

    fix import placement

--- Sources/InstantSearchSwiftUI/DataModel/SwitchIndexObservableController.swift
+++ Sources/InstantSearchSwiftUI/DataModel/SwitchIndexObservableController.swift
@@ -6,8 +6,8 @@
 //
 #if !InstantSearchCocoaPods
 import InstantSearchCore
-import InstantSearchTelemetry
 #endif
+import InstantSearchTelemetry
 #if canImport(Combine) && canImport(SwiftUI) && (arch(arm64) || arch(x86_64))
 import Combine
 import SwiftUI
