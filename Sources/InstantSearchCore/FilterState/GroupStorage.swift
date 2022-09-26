de586d7cd904d589f836c62d18d4d4a0d97e759a
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-09-27 00:18:53 +0200

    fix issues with hierarchical facets cleaning

--- Sources/InstantSearchCore/FilterState/GroupStorage.swift
+++ Sources/InstantSearchCore/FilterState/GroupStorage.swift
@@ -147,7 +147,9 @@ extension GroupsStorage: FiltersWritable {
     for groupID in groupIDs {
       switch groupID {
       case .hierarchical:
-        filterGroups[groupID] = filterGroups[groupID]?.withFilters([])
+        var cleanHierarchicalGroup = filterGroups[groupID]?.withFilters([]) as! FilterGroup.Hierarchical
+        cleanHierarchicalGroup.hierarchicalFilters.removeAll()
+        filterGroups[groupID] = cleanHierarchicalGroup
       default:
         filterGroups.removeValue(forKey: groupID)
       }
@@ -182,7 +184,7 @@ extension GroupsStorage: FiltersWritable {
   }
 
   mutating func removeAll() {
-    filterGroups.removeAll()
+    removeAll(fromGroupWithIDs: Array(getGroupIDs()))
   }
 
 }
