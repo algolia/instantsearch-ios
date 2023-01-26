c3a7367a252527e343b0d55ee029f9486ac71a8c
Niccolò Fontana <niccolo.fontana@vidiemme.it>
2023-01-26 18:58:03 +0100

    Fix: HitsList previews compiler directive

--- Sources/InstantSearchSwiftUI/View/HitsList.swift
+++ Sources/InstantSearchSwiftUI/View/HitsList.swift
@@ -68,83 +68,85 @@
     }
   }
 
-  @available(iOS 13.0, tvOS 13.0, watchOS 7.0, *)
-  struct HitsView_Previews: PreviewProvider {
-    
-    struct PreviewRecord<Value: Codable>: Codable {
-      let objectID: ObjectID
-      let value: Value
+  #if os(iOS)
+    @available(iOS 13.0, tvOS 13.0, watchOS 7.0, *)
+    struct HitsView_Previews: PreviewProvider {
       
-      init(_ value: Value, objectID: ObjectID = ObjectID(rawValue: UUID().uuidString)) {
-        self.value = value
-        self.objectID = objectID
+      struct PreviewRecord<Value: Codable>: Codable {
+        let objectID: ObjectID
+        let value: Value
+        
+        init(_ value: Value, objectID: ObjectID = ObjectID(rawValue: UUID().uuidString)) {
+          self.value = value
+          self.objectID = objectID
+        }
+        
+        static func withValue(_ value: Value) -> Self {
+          .init(value)
+        }
       }
       
-      static func withValue(_ value: Value) -> Self {
-        .init(value)
+      static let rawHits: Data = """
+          {
+            "hits": [
+              {
+              "objectID": "1",
+              "value": "h1"
+              },
+              {
+              "objectID": "2",
+              "value": "h2"
+              }
+            ]
+          }
+          """.data(using: .utf8)!
+      
+      static let hitsController: HitsObservableController<PreviewRecord<String>> = .init()
+      
+      static let interactor = HitsInteractor<PreviewRecord<String>>(infiniteScrolling: .off, showItemsOnEmptyQuery: true)
+      
+      static var previews: some View {
+        NavigationView {
+          HitsList(hitsController) { string, _ in
+            VStack {
+              HStack {
+                Text(string?.value ?? "---")
+                  .frame(maxWidth: .infinity, minHeight: 30, maxHeight: .infinity, alignment: .leading)
+                  .padding(.horizontal, 16)
+              }
+              Divider()
+            }
+          } noResults: {
+            Text("No results")
+          }
+          .padding(.top, 20)
+          .onAppear {
+            hitsController.hitsSource = interactor
+            
+            let results = try! JSONDecoder().decode(SearchResponse.self, from: rawHits)
+            
+            interactor.onResultsUpdated.subscribe(with: hitsController) { (reb, hit) in
+              reb.reload()
+            }
+            interactor.update(results)
+          }
+          .navigationBarTitle("Hits")
+        }
+        
+        NavigationView {
+          HitsList(hitsController) { string, _ in
+            VStack {
+              HStack {
+                Text(string?.value ?? "---")
+              }
+              Divider()
+            }
+          } noResults: {
+            Text("No results")
+          }
+          .navigationBarTitle("Hits")
+        }
       }
     }
-    
-    static let rawHits: Data = """
-        {
-          "hits": [
-            {
-            "objectID": "1",
-            "value": "h1"
-            },
-            {
-            "objectID": "2",
-            "value": "h2"
-            }
-          ]
-        }
-        """.data(using: .utf8)!
-    
-    static let hitsController: HitsObservableController<PreviewRecord<String>> = .init()
-    
-    static let interactor = HitsInteractor<PreviewRecord<String>>(infiniteScrolling: .off, showItemsOnEmptyQuery: true)
-    
-    static var previews: some View {
-      NavigationView {
-        HitsList(hitsController) { string, _ in
-          VStack {
-            HStack {
-              Text(string?.value ?? "---")
-                .frame(maxWidth: .infinity, minHeight: 30, maxHeight: .infinity, alignment: .leading)
-                .padding(.horizontal, 16)
-            }
-            Divider()
-          }
-        } noResults: {
-          Text("No results")
-        }
-        .padding(.top, 20)
-        .onAppear {
-          hitsController.hitsSource = interactor
-          
-          let results = try! JSONDecoder().decode(SearchResponse.self, from: rawHits)
-          
-          interactor.onResultsUpdated.subscribe(with: hitsController) { (reb, hit) in
-            reb.reload()
-          }
-          interactor.update(results)
-        }
-        .navigationBarTitle("Hits")
-      }
-      
-      NavigationView {
-        HitsList(hitsController) { string, _ in
-          VStack {
-            HStack {
-              Text(string?.value ?? "---")
-            }
-            Divider()
-          }
-        } noResults: {
-          Text("No results")
-        }
-        .navigationBarTitle("Hits")
-      }
-    }
-  }
+  #endif
 #endif
