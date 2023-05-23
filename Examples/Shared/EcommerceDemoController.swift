313a5a6c3f96c82352cdc3c437d80c8a600f21d0
Vladislav Fitc <vladislav.fitz@gmail.com>
2023-05-23 15:53:06 +0200

    the example of search query debouncing

--- Examples/Shared/EcommerceDemoController.swift
+++ Examples/Shared/EcommerceDemoController.swift
@@ -14,6 +14,8 @@ class EcommerceDemoController {
   let searchBoxConnector: SearchBoxConnector
   let statsConnector: StatsConnector
   let loadingConnector: LoadingConnector
+  
+  var lastRequestTimeStamp: TimeInterval? = nil
 
   init(searchTriggeringMode: SearchTriggeringMode = .searchAsYouType) {
     searcher = .init(client: .ecommerce,
@@ -23,5 +25,21 @@ class EcommerceDemoController {
     statsConnector = .init(searcher: searcher)
     loadingConnector = .init(searcher: searcher)
     hitsInteractor.connectSearcher(searcher)
+    
+    searcher.onSearch.subscribe(with: self) { controller, _ in
+      controller.lastRequestTimeStamp = Date().timeIntervalSince1970
+    }
+    
+    let threshold: TimeInterval = 0.5
+    searcher.shouldTriggerSearchForQuery = { query in
+      if let lastRequestTimeStamp = self.lastRequestTimeStamp {
+        let currentTimeStamp = Date().timeIntervalSince1970
+        let diff = currentTimeStamp - lastRequestTimeStamp
+        print("diff \(diff), \(diff > threshold)")
+        return diff > threshold
+      }
+      return true
+    }
+
   }
 }
