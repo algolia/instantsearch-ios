556107f7e61a3ddbb5549ab9cae54110cd6eb794
Andras <ferencz.andras@192.168.0.130>
2026-03-10 13:59:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -30,23 +30,21 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(name: "AlgoliaSearchClient",
-             url: "https://github.com/algolia/algoliasearch-client-swift",
+    .package(url: "https://github.com/algolia/algoliasearch-client-swift",
              "9.41.0"..<"10.0.0"),
     .package(url: "https://github.com/apple/swift-log",
              from: "1.5.4"),
     .package(url: "https://github.com/apple/swift-protobuf",
-             .exact("1.26.0")),
-    .package(name: "InstantSearchTelemetry",
-             url: "https://github.com/algolia/instantsearch-telemetry-native",
-             .exact("0.1.3"))
+             exact: "1.26.0"),
+    .package(url: "https://github.com/algolia/instantsearch-telemetry-native",
+             exact: "0.1.3")
   ],
   targets: [
     .target(
       name: "InstantSearchInsights",
       dependencies: [
-        .product(name: "AlgoliaCore", package: "AlgoliaSearchClient"),
-        .product(name: "AlgoliaInsights", package: "AlgoliaSearchClient")
+        .product(name: "AlgoliaCore", package: "algoliasearch-client-swift"),
+        .product(name: "AlgoliaInsights", package: "algoliasearch-client-swift")
       ],
       exclude: ["Readme.md"],
       resources: [.copy("../PrivacyInfo.xcprivacy")]
@@ -55,17 +53,17 @@ let package = Package(
       name: "InstantSearchInsightsTests",
       dependencies: [
         "InstantSearchInsights",
-        .product(name: "AlgoliaCore", package: "AlgoliaSearchClient"),
-        .product(name: "AlgoliaInsights", package: "AlgoliaSearchClient")
+        .product(name: "AlgoliaCore", package: "algoliasearch-client-swift"),
+        .product(name: "AlgoliaInsights", package: "algoliasearch-client-swift")
       ]
     ),
     .target(
       name: "InstantSearchCore",
       dependencies: [
-        .product(name: "AlgoliaCore", package: "AlgoliaSearchClient"),
-        .product(name: "AlgoliaSearch", package: "AlgoliaSearchClient"),
+        .product(name: "AlgoliaCore", package: "algoliasearch-client-swift"),
+        .product(name: "AlgoliaSearch", package: "algoliasearch-client-swift"),
         "InstantSearchInsights",
-        .product(name: "InstantSearchTelemetry", package: "InstantSearchTelemetry"),
+        .product(name: "InstantSearchTelemetry", package: "instantsearch-telemetry-native"),
         .product(name: "Logging", package: "swift-log")
       ],
       resources: [.copy("../PrivacyInfo.xcprivacy")]
@@ -75,8 +73,8 @@ let package = Package(
       dependencies: [
         "InstantSearchCore",
         "InstantSearchInsights",
-        .product(name: "AlgoliaCore", package: "AlgoliaSearchClient"),
-        .product(name: "AlgoliaSearch", package: "AlgoliaSearchClient")
+        .product(name: "AlgoliaCore", package: "algoliasearch-client-swift"),
+        .product(name: "AlgoliaSearch", package: "algoliasearch-client-swift")
       ],
       resources: [
         .copy("Misc/DisjFacetingResult1.json"),
@@ -100,7 +98,7 @@ let package = Package(
     ),
     .target(
       name: "InstantSearchSwiftUI",
-      dependencies: ["InstantSearchCore", .product(name: "InstantSearchTelemetry", package: "InstantSearchTelemetry")],
+      dependencies: ["InstantSearchCore", .product(name: "InstantSearchTelemetry", package: "instantsearch-telemetry-native")],
       resources: [.copy("../PrivacyInfo.xcprivacy")]
     ),
     .testTarget(
